a6814dc039908eb804c1ad0958df313a2e1709c9
Duc Nguyen <duc.nguyen@software.codegym.vn>
2026-06-07 11:44:19 +0700

    tạo shared package và setup alias import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
-import './App.css'
-import api from "./api/api";
+import '@/App.css'
+import api from "@/api/api";
+import ROLES from "@shared/constants/roles";
 
 function App() {
 
@@ -22,6 +23,7 @@ function App() {
         <>
             <h1>Welcome to my Salon</h1>
             <p>{message}</p>
+            <p>{ROLES.ADMIN}</p>
         </>
     );
 }
---
+++ shared/constants/roles.js
@@ -0,0 +1,7 @@
+const ROLES = {
+  ADMIN: "ADMIN",
+  CUSTOMER: "CUSTOMER",
+  STAFF: "STAFF"
+};
+
+export default ROLES;
